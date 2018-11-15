0ee9b92c4f4428504788e7a459d3f51b0dc3dd62
Darrell Walisser <darrell.walisser@gmail.com>
2018-11-15 09:10:50 -1000

    log to a udp socket

---
+++ readlog.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+socat udp-recv:2222 -
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo modprobe ipmi_devintf
-sudo hddtemp --numeric --daemon /dev/sd[b-f]
-while true; do
-     cd /home/ghaxt/src/smfanctl
-     sudo -u ghaxt ./smfanctl.py >/var/log/smfanctl/smfanctl.log 2>&1
-done
