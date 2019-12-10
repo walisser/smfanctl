3d17c49786a8b6f73b76d39f16290b945353bbc3
Darrell Walisser <darrell.walisser@gmail.com>
2019-12-10 05:50:24 -1000

    add reading smart data via areca passthrough

--- areca-hwinfo.cpp
+++ areca-hwinfo.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string>
 
 int main(int argc, char** argv)
 {
@@ -11,6 +12,22 @@ int main(int argc, char** argv)
     {
        printf("%s must be run as root\n", argv[0]);
        return -1;
+    }
+
+    if (argc > 1 && std::string(argv[1])=="-disk-info")
+       return system("./cli64 disk info");
+    if (argc > 1 && std::string(argv[1])=="-disk-smart")
+    {
+       std::string cmd = "echo -e \"";
+       for (int i = 0; i < argc; i++)
+       {
+          cmd += "disk smart drv=";
+          cmd += argv[i];
+          cmd += "\n";
+       }
+       cmd += "exit";
+       cmd += "\" | ./cli64";
+       return system(cmd.c_str());
     }
 
     return system("./cli64 hw info");
